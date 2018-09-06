697b8e7a37070bdf69085056f4583c86683ee55e
aappleg <42657805+aappleg@users.noreply.github.com>
2018-09-05 20:26:26 -0400

    Add files via upload

---
+++ src/step1.java
@@ -0,0 +1,34 @@
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+import java.io.IOException;
+
+public class step1 {
+	public static void main(String[] args) throws Exception {
+		ANTLRFileStream stream = new ANTLRFileStream(args[0]);
+		step1Lexer lex = new step1Lexer(stream);
+		Token t = lex.nextToken();
+		while(t.getType() != Token.EOF) {
+			if (t.getType() == step1Lexer.KEYWORD) {
+				System.out.print("KEYWORD");
+			}
+			else if (t.getType() == step1Lexer.IDENTIFIER) {
+				System.out.print("IDENTIFIER");
+			}
+			else if (t.getType() == step1Lexer.INTLITERAL) {
+				System.out.print("INTLITERAL");
+			}
+			else if (t.getType() == step1Lexer.FLOATLITERAL) {
+				System.out.print("FLOATLITERAL");
+			}
+			else if (t.getType() == step1Lexer.OPERATOR) {
+				System.out.print("OPERATOR");
+			}
+			else if (t.getType() == step1Lexer.STRINGLITERAL) {
+				System.out.print("STRINGLITERAL");
+			}
+			
+			System.out.println(" (Value = \"" + t.getText() + "\")");
+			t = lex.nextToken();
+		}
+	}
+}
