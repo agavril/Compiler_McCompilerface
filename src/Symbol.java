ba4e74edae0f43a697fbc467f16217c98b208891
agavril <agavril@purdue.edu>
2018-10-02 16:09:00 -0400

    Just pushin

---
+++ src/Symbol.java
@@ -0,0 +1,12 @@
+import org.antlr.v4.runtime.*;
+import java.util.*;
+
+public class Symbol{
+	String name;
+	String type;
+	String value;
+	
+	public void print (){
+
+	}
+}
--- src/SymbolTable.java
+++ src/SymbolTable.java
@@ -1,12 +1,9 @@
 import org.antlr.v4.runtime.*;
+import java.util.*;
 
-public class Symbol{
-	String name;
-	String type;
-	String value;
-}
-	
-public class Semantics{
+public class SymbolTable{
 	public String scope;
 	ArrayList<Symbol> symbols = new ArrayList<Symbol>();
+	
+	public void print (){
 }
