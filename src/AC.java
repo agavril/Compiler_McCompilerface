808b455ae27bfcc9993f73b5927908a2f2ab4950
agavril <agavril@purdue.edu>
2018-10-13 12:53:30 -0400

    Added lists and finished switch

--- src/AC.java
+++ src/AC.java
@@ -7,89 +7,108 @@ public class AC{
 	
 }
 
+class ACList extends LinkedList{
+	public ACList(){}
+}
+
 class ACNode{
 	public String opname;
-	public String src1;
-	public String src2;
+	public String op1;
+	public String op2;
 	public String dest;	
-	public ACNode(String opname, String src1, String src2, String dest){
+	public ACNode(String opname, String op1, String op2, String dest){
 		this.opname = opname;
-		this.src1 = src1;
-		this.src2 = src2;
+		this.src1 = op1;
+		this.src2 = op2;
 		this.dest = dest;
 	}
 	
-	public String ACToTiny(){
-		String tinySrc1 = varToReg(src1);
-		String tinySrc2 = varToReg(src2);
+	public TinyList ACToTiny(){		//Converts an ACNode to a TinyNode
+		TinyList newList = new TinyList();
+		String tinyOp1 = varToReg(op1);
+		String tinyOp2 = varToReg(op2);
 		String tinyDest = varToReg(dest);
 		switch(opname){
 			case("ADDI"):
-			
+				TinyNode new1 = new TinyNode("move", tinyOp1, tinyOp2);
+				TinyNode new2 = new TinyNode("addi", tinyOp2, tinyDest);
+				newList.addLast(new1);
+				newList.addLast(new2);				
 			case("SUBI"):
-			
+				TinyNode new1 = new TinyNode("move", tinyOp1, tinyOp2);
+				TinyNode new2 = new TinyNode("subi", tinyOp2, tinyDest);
+				newList.addLast(new1);
+				newList.addLast(new2);
 			case("MULI"):
-		
+				TinyNode new1 = new TinyNode("move", tinyOp1, tinyOp2);
+				TinyNode new2 = new TinyNode("muli", tinyOp2, tinyDest);
+				newList.addLast(new1);
+				newList.addLast(new2);
 			case("DIVI"):
-			
+				TinyNode new1 = new TinyNode("move", tinyOp1, tinyOp2);
+				TinyNode new2 = new TinyNode("divi", tinyOp2, tinyDest);
+				newList.addLast(new1);
+				newList.addLast(new2);
 			case("ADDF"):
-			
+				TinyNode new1 = new TinyNode("move", tinyOp1, tinyOp2);
+				TinyNode new2 = new TinyNode("addr", tinyOp2, tinyDest);
+				newList.addLast(new1);
+				newList.addLast(new2);
 			case("SUBF"):
-			
+				TinyNode new1 = new TinyNode("move", tinyOp1, tinyOp2);
+				TinyNode new2 = new TinyNode("subr", tinyOp2, tinyDest);
+				newList.addLast(new1);
+				newList.addLast(new2);
 			case("MULF"):
-			
+				TinyNode new1 = new TinyNode("move", tinyOp1, tinyOp2);
+				TinyNode new2 = new TinyNode("mulr", tinyOp2, tinyDest);
+				newList.addLast(new1);
+				newList.addLast(new2);
 			case("DIVF"):
-			
-			case("STOREI"):
-			
-			case("STOREF"):
-			
+				TinyNode new1 = new TinyNode("move", tinyOp1, tinyOp2);
+				TinyNode new2 = new TinyNode("divr", tinyOp2, tinyDest);
+				newList.addLast(new1);
+				newList.addLast(new2);
+			case("STOREI"): case("STOREF"):
+				TinyNode new1 = new TinyNode("move", tinyOp1, tinyDest);
+				newList.addLast(new1);
 			case("READI"):
-			
+				TinyNode new1 = new TinyNode("sys readi", tinyDest, null);
+				newList.addLast(new1);
 			case("READF"):
-			
+				TinyNode new1 = new TinyNode("sys readr", tinyDest, null);
+				newList.addLast(new1);
 			case("WRITEI"):
-			
+				TinyNode new1 = new TinyNode("sys writei", tinyDest, null);
+				newList.addLast(new1);
 			case("WRITEF"):
-			
+				TinyNode new1 = new TinyNode("sys writer", tinyDest, null);
+				newList.addLast(new1);
 			case("WRITES"):
-		
-		}
-		ADDI  OP1 OP2 RESULT (Integer add; RESULT = OP1 + OP2)
-		SUBI  OP1 OP2 RESULT (Integer sub; RESULT = OP1 - OP2)
-		MULI  OP1 OP2 RESULT (Integer mul; RESULT = OP1 * OP2)
-		DIVI  OP1 OP2 RESULT (Integer div; RESULT = OP1 / OP2)
-
-		ADDF  OP1 OP2 RESULT (Floating point add; RESULT = OP1 + OP2)
-		SUBF  OP1 OP2 RESULT (Floating point sub; RESULT = OP1 - OP2)
-		MULF  OP1 OP2 RESULT (Floating point mul; RESULT = OP1 * OP2)
-		DIVF  OP1 OP2 RESULT (Floating point div; RESULT = OP1 / OP2)
-
-		STOREI OP1 RESULT (Integer store; store OP1 in RESULT)
-		STOREF OP1 RESULT (Floating point store; store OP1 in RESULT)
-
-		READI RESULT (Read integer from console; store in RESULT)
-		READF RESULT (Read float from console; store in RESULT)
-
-		WRITEI OP1 (Write integer OP1 to console)
-		WRITEF OP1 (Write float OP1 to console)
-		WRITES OP1 (Write string OP1 to console)		
-		
-		
-		return null;
+				TinyNode new1 = new TinyNode("sys writef", tinyDest, null);
+				newList.addLast(new1);
+			default:
+				System.out.printf("Not a valid IR");
+				return null;
+		}		
+		return newList;
 	}
 	
 	public String varToReg(String var){
 		if(var == null){return null;}
 		String result;
 		if(var.startsWith("!")){
-			result = "r" + Integer.parseInt(var.substring(1,var.length()-1));
+			result = "r" + (Integer.parseInt(var.substring(1,var.length()-1))-1);
 			return result;
 		}
 		else{return var;}
 	}
 		
 		
+}
+
+class TinyList extends LinkedList{
+	public TinyList(){}
 }
 
 class TinyNode{
